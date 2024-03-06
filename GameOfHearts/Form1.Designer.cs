0d3c222f920adc0fece3e2d3215a30579c81318f
0Pierre <151689805+0Pierre@users.noreply.github.com>
2024-03-06 12:08:58 -0500

    btn2

--- GameOfHearts/Form1.Designer.cs
+++ GameOfHearts/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -40,11 +41,21 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(292, 64);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "GameOfHearts";
@@ -55,5 +66,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
