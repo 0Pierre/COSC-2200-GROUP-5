dcf19e67cf8a43da41bb3010935c904f3279bf5a
0Pierre <151689805+0Pierre@users.noreply.github.com>
2024-03-12 19:24:35 -0400

    start of actual form

--- GameOfHearts/Form2.Designer.cs
+++ GameOfHearts/Form2.Designer.cs
@@ -30,19 +30,34 @@
         {
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
-            checkBox1 = new CheckBox();
             contextMenuStrip1 = new ContextMenuStrip(components);
+            gbMain = new GroupBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            gbMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
             SuspendLayout();
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(201, 107);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(101, 24);
-            checkBox1.TabIndex = 0;
-            checkBox1.Text = "checkBox1";
-            checkBox1.UseVisualStyleBackColor = true;
             // 
             // contextMenuStrip1
             // 
@@ -51,12 +66,198 @@
             contextMenuStrip1.RenderMode = ToolStripRenderMode.System;
             contextMenuStrip1.Size = new Size(61, 4);
             // 
+            // gbMain
+            // 
+            gbMain.BackColor = Color.Transparent;
+            gbMain.Controls.Add(label8);
+            gbMain.Controls.Add(label5);
+            gbMain.Controls.Add(label7);
+            gbMain.Controls.Add(label6);
+            gbMain.Controls.Add(pictureBox5);
+            gbMain.Controls.Add(pictureBox8);
+            gbMain.Controls.Add(pictureBox7);
+            gbMain.Controls.Add(label4);
+            gbMain.Controls.Add(label3);
+            gbMain.Controls.Add(label2);
+            gbMain.Controls.Add(label1);
+            gbMain.Controls.Add(pictureBox4);
+            gbMain.Controls.Add(pictureBox3);
+            gbMain.Controls.Add(pictureBox2);
+            gbMain.Controls.Add(pictureBox1);
+            gbMain.Controls.Add(pictureBox6);
+            gbMain.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            gbMain.ForeColor = SystemColors.Control;
+            gbMain.Location = new Point(12, 12);
+            gbMain.Name = "gbMain";
+            gbMain.Size = new Size(1292, 608);
+            gbMain.TabIndex = 1;
+            gbMain.TabStop = false;
+            gbMain.Text = "Score";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(41, 70);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(83, 91);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(41, 458);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(83, 91);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(41, 331);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(83, 91);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(41, 202);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(83, 91);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 28);
+            label1.TabIndex = 4;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(49, 174);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 28);
+            label2.TabIndex = 5;
+            label2.Text = "Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(49, 301);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 28);
+            label3.TabIndex = 6;
+            label3.Text = "Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(49, 429);
+            label4.Name = "label4";
+            label4.Size = new Size(68, 28);
+            label4.TabIndex = 7;
+            label4.Text = "Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Maroon;
+            label5.Location = new Point(145, 103);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 28);
+            label5.TabIndex = 8;
+            label5.Text = "0";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Maroon;
+            label6.Location = new Point(145, 235);
+            label6.Name = "label6";
+            label6.Size = new Size(24, 28);
+            label6.TabIndex = 9;
+            label6.Text = "0";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Maroon;
+            label7.Location = new Point(145, 367);
+            label7.Name = "label7";
+            label7.Size = new Size(24, 28);
+            label7.TabIndex = 10;
+            label7.Text = "0";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Maroon;
+            label8.ForeColor = Color.Transparent;
+            label8.Location = new Point(145, 494);
+            label8.Name = "label8";
+            label8.Size = new Size(24, 28);
+            label8.TabIndex = 11;
+            label8.Text = "0";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(130, 90);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(54, 53);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 12;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(130, 480);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(54, 53);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 13;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(130, 352);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(54, 53);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 14;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(130, 224);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(54, 53);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 15;
+            pictureBox8.TabStop = false;
+            // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1231, 653);
-            Controls.Add(checkBox1);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1316, 632);
+            Controls.Add(gbMain);
             Enabled = false;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MinimizeBox = false;
@@ -65,13 +266,37 @@
             Text = "GameOfHearts";
             WindowState = FormWindowState.Maximized;
             Load += Form2_Load;
+            gbMain.ResumeLayout(false);
+            gbMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private CheckBox checkBox1;
         private ContextMenuStrip contextMenuStrip1;
+        private GroupBox gbMain;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox6;
     }
 }
